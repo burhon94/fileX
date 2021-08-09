04d91d7a3064f80b4288ced0e4b3bda78044c78b
burhon94 <tohirov1994@gmail.com>
2021-08-09 18:06:05 +0500

    swagger

--- cmd/fileX/router/router.go
+++ cmd/fileX/router/router.go
@@ -2,14 +2,23 @@ package router
 
 import (
 	"github.com/burhon94/fileX/cmd/fileX/handlers"
+	_ "github.com/burhon94/fileX/docs"
 	newRouter "github.com/burhon94/fileX/pkg/router"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"net/http"
 )
 
+//InitRoute
+// @title file-X Project API
+// @version 0.0.2
+// @host localhost:9999
+// @BasePath /
 func InitRoute(addr string) {
 	router := newRouter.NewRouter()
 
 	router.GET("/ping", handlers.PingHandler)
+
+	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler).Methods("GET")
 
 	server := http.Server{Addr: addr, Handler: router.GetServeHTTP()}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/burhon94/fileX
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/http-swagger v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+)
